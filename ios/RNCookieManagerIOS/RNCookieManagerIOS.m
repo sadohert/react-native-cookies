dd56346526283e56e7bf80dfa3120656738a36eb
Miguel Alatzar <this.migbot@gmail.com>
2020-02-13 12:03:19 -0700

    Export only one getAll method

--- ios/RNCookieManagerIOS/RNCookieManagerIOS.m
+++ ios/RNCookieManagerIOS/RNCookieManagerIOS.m
@@ -225,24 +225,6 @@ RCT_EXPORT_METHOD(
     }
 }
 
-RCT_EXPORT_METHOD(getAll:(RCTPromiseResolveBlock)resolve
-    rejecter:(RCTPromiseRejectBlock)reject) {
-    NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    NSMutableDictionary *cookies = [NSMutableDictionary dictionary];
-    for (NSHTTPCookie *c in cookieStorage.cookies) {
-        NSMutableDictionary *d = [NSMutableDictionary dictionary];
-        [d setObject:c.value forKey:@"value"];
-        [d setObject:c.name forKey:@"name"];
-        [d setObject:c.domain forKey:@"domain"];
-        [d setObject:c.path forKey:@"path"];
-        NSString *expires = [self.formatter stringFromDate:c.expiresDate];
-        if (expires != nil) {
-            [d setObject:expires forKey:@"expiresDate"];
-        }
-        [cookies setObject:d forKey:c.name];
-    }
-}
-
 -(NSDictionary *)createCookieList:(NSArray<NSHTTPCookie *>*)cookies
 {
     NSMutableDictionary *cookieList = [NSMutableDictionary dictionary];
